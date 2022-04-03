1fad2353126c9f17a690a251aba10377e2b973e3
duytruong09 <75570501+duytruong09@users.noreply.github.com>
2022-04-03 18:25:15 +0700

    Buoi 2

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # ĐÂY LÀ CHƯƠNG TRINH SOCKET.IO
 
-#
-
 ## Chương trình sử dụng NodeJS là chính
--- server.js
+++ server.js
@@ -17,9 +17,26 @@ app.get('/', (req, res) => {
 })
 
 io.on('connection', (socket) => {
-  console.log('a user connect' + socket.id)
+  console.log('a user connect ' + socket.id)
+
   socket.on('disconnect', function () {
     console.log(socket.id + 'is disconnected!!!')
+  })
+
+  socket.on('Client-send-data', (data) => {
+    console.log(socket.id + 'send: ' + data)
+
+    // Gui lai nguoi vua gui
+    // socket.emit('Server-send-data', `Client ${socket.id} send: ` + data)
+
+    // Gui lai tat ca moi nguoi dang ket noi
+    io.sockets.emit('Server-send-data', data)
+
+    // Gui lai nguoi khac tru nguoi gui
+    // socket.broadcast.emit('Server-send-data', `Client ${socket.id} send: ` + data)
+
+    // Gui den nguoi cu the
+    // io.to('SGW-toRMKdB13PBqAAAD').emit('Server-send-data', `Client ${socket.id} send: ` + data)
   })
 })
 
